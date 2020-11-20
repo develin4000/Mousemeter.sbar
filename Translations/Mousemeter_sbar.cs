28f75c5bff7ee2666c5d321184a30690d00eaeda
Stefan Blixth <stefan@onyxsoft.se>
2020-11-20 12:25:19 +0100

    Added Turkish translation, thanks to Serkan Dursun

--- Translations/Mousemeter_sbar.cs
+++ Translations/Mousemeter_sbar.cs
@@ -1,79 +1,95 @@
 ## Version $VER: Mousemeter_sbar.catalog 1.3 (19.11.2020)
-## Languages english svenska deutsch
+## Languages english svenska deutsch türkçe
 ## Codeset english 0
 ## Codeset svenska 0
 ## Codeset deutsch 0
+## Codeset türkçe 0
 ## SimpleCatConfig CharsPerLine 200
 ## Chunk svenska AUTH Stefan Blixth
 ## Chunk deutsch AUTH Stefan A. Haubenthal
+## Chunk türkçe AUTH Serkan Dursun
 ;
 MSG_APPLICATION_NAME (0//)
 Mousemeter
 Mousemeter
 Mausmeter
+Fare mesafesi
 ;
 MSG_GUI_LABEL_VERTICAL (1//)
 Vertical:
 Vertikal:
 Vertikal:
+Dikey:
 ;
 MSG_GUI_LABEL_HORIZONTAL (2//)
 Horizontal:
 Horizontal:
 Horizontal:
+Yatay:
 ;
 MSG_GUI_LABEL_TOTAL (3//)
 Total:
 Totalt:
 Insgesamt:
+Toplam:
 ;
 MSG_GUI_LABEL_AVERAGESPEED (4//)
 Average Speed:
 Medelhastighet:
 Durchschnittliche Geschwindigkeit:
+Avaraj Hýzý:
 ;
 MSG_GUI_SHORTHELP_BUTTONRESET (5//)
 Resets the statistics data.
 Nollställer statistisk data.
 Setzt die Statistikdaten zurück.
+Ýstatistik verilerini sýfýrla.
 ;
 MSG_SETTINGS_LABEL_MONITORSIZE (6//)
 Monitor Size:
 Bildskärmsstorlek:
 Monitorgröße:
+Monitör Boyutu:
 ;
 MSG_SETTINGS_LABEL_UNIT (7//)
 Unit:
 Enhet:
 Einheit:
+Birim:
 ;
 MSG_SETTIGS_SLIDER_INCH (8//)
 %ld inch
 %ld tum
 %ld Zoll
+%ld inc
 ;
 MSG_SETTINGS_LABEL_SHOWONSTARTUP (9//)
 Show window on start
 Öppna fönster vid start
 Fenster beim Start anzeigen
+Pencereyi baþlangýçta göster
 ;
 MSG_GUI_LABEL_MOUSECLICKS (10//)
 Mouse Clicks:
 Musklick:
 Mausklicks:
+Fare Týklamasý:
 ;
 MSG_SETTINGS_LABEL_SHOWCLICKS (11//)
 Show mouse clicks
 Visa musklick
 Mausklicks anzeigen
+Fare týklamasýný gösterir
 ;
 MSG_GUI_LABEL_WHEELMOUSE (12//)
 Wheel Moves:
 Hjulrörelser:
 Radbewegungen:
+Teker Hareketleri:
 ;
 MSG_SETTINGS_LABEL_SHOWWHEEL (13//)
 Show mouse wheel moves
 Visa hjulmusrörelser
 Bewegungen des Mausrads anzeigen
+Teker hareketlerini gösterir
 ;
